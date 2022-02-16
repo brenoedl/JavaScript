2b0096159828467f21b1b3dda9d58015bafaefc5
Breno170 <96654144+Breno170@users.noreply.github.com>
2022-02-15 22:12:01 -0300

    gb

---
+++ exercicios/ex0004/ex004-jss/a.js
@@ -0,0 +1,6 @@
+var vel = 82
+console.log(`A velocidadde do seu veiculo é de ${vel}Km/h`)
+if(vel > 80){
+    console.log('Você utrapassou o limite da velocidade permitida MULTADO')
+}
+console.log('Dirija sempre com o sinto de segurança')
