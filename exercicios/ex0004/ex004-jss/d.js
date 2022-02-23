02fc01841c626899c501281ce43aabe5b36267d2
Breno170 <96654144+Breno170@users.noreply.github.com>
2022-02-23 06:49:27 -0300

    gffv

---
+++ exercicios/ex0004/ex004-jss/d.js
@@ -0,0 +1,27 @@
+var agoora = new Date()
+var diasem = agoora.getDay()
+switch(diasem){
+    case 0:
+        console.log('Domingo')
+        break
+    case 1:
+        console.log('Segunda')
+        break
+    case 2:
+        console.log('Terça')
+        break
+    case 3:
+        console.log('Quarta')
+        break
+    case 4:
+        console.log('Quita')
+        break
+    case 5:
+        console.log('Sexta')
+        break
+    case 6:
+        console.log('Sabado')
+        break
+    default:
+        console.log('[ERRO] Dia invalido')
+}
