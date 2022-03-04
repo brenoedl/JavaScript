5f2ab4a1c550014aaa9fe161b048f1c7a5c043a9
Breno170 <96654144+Breno170@users.noreply.github.com>
2022-03-04 10:15:59 -0300

    17

---
+++ desafios/des001/des001-p01/interatvidade.js
@@ -0,0 +1,19 @@
+function caregar(){
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são exatamente ${hora} horas`
+    if(hora >= 0 && hora < 12){
+        //bom dia
+        document.body.style.background = '#e2cd9f'
+    }else if(hora >= 12 && hora <= 18){
+        //boa tarde
+        img.src = 'tarde.png'
+        document.body.style.background = '#b9846f'
+    }else{
+        //boa noite
+        img.src = 'noite.png'
+        document.body.style.background = '#515154'
+    }
+}
