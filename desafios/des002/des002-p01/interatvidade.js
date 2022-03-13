6bd69a1986e215525b008f385f2796e12fe788c7
Breno170 <96654144+Breno170@users.noreply.github.com>
2022-03-13 16:45:30 -0300

    iuuij

--- desafios/des002/des002-p01/interatvidade.js
+++ desafios/des002/des002-p01/interatvidade.js
@@ -1,6 +1,17 @@
 function contar(){
     let ini = document.getElementById('txt-i')
     let fim = document.getElementById('txt-f')
-    let paso = document.getElementById('txt-p')
-    
+    let passo = document.getElementById('txt-p')
+    let res = document.querySelector('div#res')
+
+    if(ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0 ){
+        alert('[ERRO] faltam dados')
+    }else{
+        res.innerHTML = 'Contamdo: <br>'
+        var i = Number(ini.value)
+        var f = Number(fim.value)
+        var p = Number(passo.value)
+
+        for(var c = i; c <= f; c +=)
+    }
 }
