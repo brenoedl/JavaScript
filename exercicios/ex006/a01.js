2cb2997f5a68d0a4cc4f39393fa6cd293430a0de
Breno170 <96654144+Breno170@users.noreply.github.com>
2022-03-15 20:47:19 -0300

    mobp

---
+++ exercicios/ex006/a01.js
@@ -0,0 +1,6 @@
+var m = [1,5,6,8]
+m[4] = 7
+m.push(3)
+console.log(`o meu vetor é o ${m} ele tem ${m.length} posições e ele organisaddo fica assim ${m.sort()}`)
+let o = m.indexOf(2)
+console.log(`o numero 2 está na posição ${o}`)
---
+++ exercicios/ex006/a02.js
@@ -0,0 +1,4 @@
+let b = [2,4,6,1,0,5,7,9,8]
+for(var p in b){
+    console.log(`a posição ${p} tem o numero ${b[p]}`)
+}
